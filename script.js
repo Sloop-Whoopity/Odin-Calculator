62c18526263b2d5ac610b6e681ea4e8f0a478d15
elijah_ubuntu <elijah.pitt@proton.me>
2024-04-03 00:41:18 -0600

    Add numbers and operators classes and listeners
    Added code to add numbers and operators classes to the html through js.
    Add event listeners to the buttons to add functionality.

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let operator;
+let display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+let regex = /[0-9]/; //Here we define the regular expression.  regex is all lowercase since it is an actual word
+let operEx = /[^0-9]/; //This means any non-digit character.
+
+console.log(regex.test(5))
+
+
+buttons.forEach((button) => {
+    if (regex.test(button.textContent)) { //We use a regular expression to match if a character is a number between 0-9.  Number function doesn't work because it was translating special characters as numbers as well
+        console.log("These are the number buttons", button.textContent);
+        button.classList.add("numbers");
+    }
+});
+
+const numButtons = document.querySelectorAll(".numbers")
+
+numButtons.forEach((button) => button.addEventListener("click", () => {
+    // display.textContent = button.textContent;
+    if (operator == null) {  //Not strictly equal since operator may start undefined
+        firstNumber === 0 ? firstNumber = button.textContent :
+        firstNumber += button.textContent;
+            }
+     }
+    )
+);
+
+buttons.forEach((button) => {
+    if (operEx.test(button.textContent)) {
+        button.classList.add("operators");
+        console.log("Operator ", button.textContent)
+    }
+});
+
+const operButtons = document.querySelectorAll(".operators")
+
+operButtons.forEach((button) => button.addEventListener("click", () => {
+    if (button.textContent === "=") {
+        if (secondNumber === "0") {
+            return firstNumber
+        } else {
+            operate(+firstNumber, +operator, +secondNumber);
+        }
+    } else {
+        operator = button.textContent;
+    }
+    }
+));
+
 
 
 function add(a,b) {
@@ -30,3 +79,14 @@ function operate(number1, operatorSign, number2) {
        return divide(number1, number2);
     }
 }
+
+function displayText() {
+    display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+}
+
+
+    // if (button.classList === "numbers") {
+    // button.addEventListener("click", () => {
+    // display.textContent = button.textContent;
+    // }
+    // )}   
