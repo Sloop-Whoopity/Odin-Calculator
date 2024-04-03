97d929e298d5106d350ac8ef8e1c244256489cb5
elijah_ubuntu <elijah.pitt@proton.me>
2024-04-03 13:44:09 -0600

    Implement operate() and display functionality
    Added functionality to translate button clicks into firstNumber and
    secondNumber variables.  Implemented the operate() function so that
    it operates based on the variables.  Created the displayText() function
    so that the user input is displayed to user.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 let regex = /[0-9]/; //Here we define the regular expression.  regex is all lowercase since it is an actual word
 let operEx = /[^0-9]/; //This means any non-digit character.
+let result;
+
 
 console.log(regex.test(5))
 
@@ -23,8 +25,12 @@ numButtons.forEach((button) => button.addEventListener("click", () => {
     if (operator == null) {  //Not strictly equal since operator may start undefined
         firstNumber === 0 ? firstNumber = button.textContent :
         firstNumber += button.textContent;
-            }
-     }
+    } else if (operator != null && firstNumber != 0) {
+        secondNumber === 0 ? secondNumber = button.textContent :
+        secondNumber += button.textContent;
+        }
+    displayText();
+    }
     )
 );
 
@@ -42,8 +48,13 @@ operButtons.forEach((button) => button.addEventListener("click", () => {
         if (secondNumber === "0") {
             return firstNumber
         } else {
-            operate(+firstNumber, +operator, +secondNumber);
+            operate(+firstNumber, operator, +secondNumber);
         }
+    } else if (button.textContent === "C") {
+        firstNumber = 0;
+        operator = null;
+        secondNumber = 0;
+        display.textContent = firstNumber;
     } else {
         operator = button.textContent;
     }
@@ -54,35 +65,42 @@ operButtons.forEach((button) => button.addEventListener("click", () => {
 
 function add(a,b) {
     return a + b;
-}
+};
 
 function subtract(a,b) {
     return a - b;
-}
+};
 
 function multiply(a,b) {
     return a * b;
-}
+};
 
 function divide(a,b) {
     return a/b;
-}
+};
 
 function operate(number1, operatorSign, number2) {
     if (operatorSign === "+") {
-       return add(number1,number2);
+       result = add(number1,number2);
     } else if (operatorSign === "-") {
-       return subtract(number1, number2);
+       result = subtract(number1, number2);
     } else if (operatorSign === "*") {
-       return multiply(number1, number2); 
+       result = multiply(number1, number2); 
     } else if (operatorSign === "/") {
-       return divide(number1, number2);
+       result = divide(number1, number2);
     }
+    result = Math.round(result * 100000)/100000; // Curently rounding numbers for display purposes
+    display.textContent = result;
+    return result;
 }
 
 function displayText() {
-    display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
-}
+    if (secondNumber === 0 && operator == null) {
+        display.textContent = firstNumber;
+    } else {
+        display.textContent = secondNumber;
+    }
+};
 
 
     // if (button.classList === "numbers") {
