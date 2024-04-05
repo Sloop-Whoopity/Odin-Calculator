56257729f19b60967e6f0116efdffd51c8c0feaa
elijah_ubuntu <elijah.pitt@proton.me>
2024-04-05 02:38:01 -0600

    Add multiple operations feature
    Configured code to allow for a sequence of math operations, the result
    of which is returned when a user selects the "=" button.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let regex = /[0-9]/; //Here we define the regular expression.  regex is all lowe
 let operEx = /[^0-9]/; //This means any non-digit character.
 let result;
 let array1 = []
+let operatorInput = 0;
+
 
 console.log(regex.test(5))
 
@@ -22,16 +24,16 @@ const numButtons = document.querySelectorAll(".numbers")
 
 numButtons.forEach((button) => button.addEventListener("click", () => {
     // display.textContent = button.textContent;
-    // if (operator == null) {  //Not strictly equal since operator may start undefined
-    //     firstNumber === 0 ? firstNumber = button.textContent :
-    //     firstNumber += button.textContent;
-    // } else if (operator != null && firstNumber != 0) {
-    //     secondNumber === 0 ? secondNumber = button.textContent :
-    //     secondNumber += button.textContent;
-    //     }
-    // displayText();
-    firstNumber === 0 ? firstNumber = button.textContent :
-    firstNumber += button.textContent;
+    if (operator == null) {  //Not strictly equal since operator may start undefined
+        firstNumber === 0 ? firstNumber = button.textContent :
+        firstNumber += button.textContent;
+    } else if (operator != null && firstNumber != 0) {
+        secondNumber === 0 ? secondNumber = button.textContent :
+        secondNumber += button.textContent;
+        }
+    displayText();
+    // firstNumber === 0 ? firstNumber = button.textContent :
+    // firstNumber += button.textContent;
     }
     )
 );
@@ -46,23 +48,17 @@ buttons.forEach((button) => {
 const operButtons = document.querySelectorAll(".operators")
 
 operButtons.forEach((button) => button.addEventListener("click", () => {
-    array1.push(firstNumber)
-    if (button.textContent === "=") {
-        if (secondNumber === "0") {
-            return firstNumber
-        } else {
-            operate(+firstNumber, operator, +secondNumber);
-        }
-    } else if (button.textContent === "C") {
+    if (button.textContent === "C") {
         firstNumber = 0;
         operator = null;
         secondNumber = 0;
         display.textContent = firstNumber;
-    } else {
-        operator = button.textContent;
-        array1.push(button.textContent);
-        firstNumber = 0;
-    }
+        return
+    } else if (operator != null) {
+        operate(+firstNumber, operator, +secondNumber);
+        };
+    
+    operator = button.textContent;  //Have the button that triggers the operate function change the operator after the calculation or set operator if operator is null.
     }
 ));
 
@@ -88,19 +84,20 @@ function operate(number1, operatorSign, number2) {
     if (operatorSign === "+") {
        firstNumber = add(number1,number2);
     } else if (operatorSign === "-") {
-       result = subtract(number1, number2);
+       firstNumber = subtract(number1, number2);
     } else if (operatorSign === "*") {
-       result = multiply(number1, number2); 
+       firstNumber = multiply(number1, number2); 
     } else if (operatorSign === "/") {
-       result = divide(number1, number2);
+       firstNumber = divide(number1, number2);
     }
-    result = Math.round(result * 100000)/100000; // Curently rounding numbers for display purposes
-    display.textContent = result;
-    return result;
+    firstNumber = Math.round(firstNumber * 100000)/100000; // Curently rounding numbers for display purposes
+    display.textContent = firstNumber;
+    secondNumber = 0;
+    return firstNumber;
 }
 
 function displayText() {
-    if (secondNumber === 0 && operator == null) {
+    if (secondNumber === 0) { //&& operator == null
         display.textContent = firstNumber;
     } else {
         display.textContent = secondNumber;
