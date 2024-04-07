98a7fdaea838203640efee12b54759b87350b4b0
elijah_ubuntu <elijah.pitt@proton.me>
2024-04-06 19:04:37 -0600

    Add decimal, negative, & percentage functionality
    Added complete functionality to use decimals, add negative signs,
    and convert number into a percent equivalent.

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ let secondNumber = 0;
 let operator;
 let display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-let regex = /[0-9.]/; //Here we define the regular expression.  regex is all lowercase since it is an actual word.  Also having regex = . for number purposes.
-let operEx = /[^0-9.]/; //This means any non-digit character.
+let regex = /[0-9.%]|\+\/\-/;
+let operEx = /[^0-9.%]/;
 let operatorInput = 0;  //Used to track if user has selected operator more than once and prevent unintended operate() call based on secondNumber resetting to 0;
 let decimalCount = 0;
-
-console.log(regex.test(5)) //Just a test for understanding regex.  Keeping the code in for memories.
-
+let firstNumberBoolean = false; //Detects whether the currently displayed number is the first or second number.
 
 buttons.forEach((button) => {
     if (regex.test(button.textContent)) { //We use a regular expression to match if a character is a number between 0-9.  Number function doesn't work because it was translating special characters as numbers as well
@@ -29,19 +27,36 @@ const numButtons = document.querySelectorAll(".numbers")
 const operButtons = document.querySelectorAll(".operators")
 
 numButtons.forEach((button) => button.addEventListener("click", () => {
-    if (button.textContent === ".") {
-            if (decimalCount > 0) {
-            console.log("You used too many decimals.  REturning!")
-            return;
-            } else {
-                decimalCount += 1;
-            }
-        }
+    switch (button.textContent) {
+        case ".":
+            if (decimalCount > 0) { //Decimal count tracks how many decimals have been inserted into a number.  Resets when an operator is selected.
+                console.log("You used too many decimals.  REturning!")
+                return;
+                } else {
+                    decimalCount += 1;
+                }
+            break;
+        
+        case "+/-":  //Return function, because the +/- button doesn't represent a value that can be passed to the rest of the function.
+        firstNumberBoolean === true ? firstNumber = firstNumber * -1 :
+        secondNumber = secondNumber * -1;
+        displayText();
+        return;
+
+        case "%":  //Return function, because the % button doesn't represent a value that can be passed to the rest of the function.
+        firstNumberBoolean === true ? firstNumber = firstNumber/100 : 
+        secondNumber = secondNumber/100;
+        displayText();
+        return;
+
+    };
     if (operator != null) {
+        firstNumberBoolean = false;
         secondNumber === 0 ? secondNumber = button.textContent :
         secondNumber += button.textContent;
-        operatorInput = 0;
+        operatorInput = 0; //Resets operator input once a second number is inserted.
     } else {
+        firstNumberBoolean = true;
         firstNumber === 0 ? firstNumber = button.textContent :
         firstNumber += button.textContent;
     }
@@ -50,22 +65,27 @@ numButtons.forEach((button) => button.addEventListener("click", () => {
 ));
 
 operButtons.forEach((button) => button.addEventListener("click", () => {
-    operatorInput += 1;
-    decimalCount = 0;
-    if (button.textContent === "CE") {
+    switch (button.textContent) {
+        case "+/-": //Unable to exclude "+/-" from operators list so telling function to ignore button if detected.
+        return;
+
+        case "CE": 
         firstNumber = 0;
         operator = null;
         secondNumber = 0;
         display.textContent = firstNumber;
-        return
-    } else if (operator != null && operatorInput === 1) {
+        decimalCount = 0;
+        return;
+    };
+    operatorInput += 1;
+    decimalCount = 0;
+    if (operator != null && operatorInput === 1) {
         operate(+firstNumber, operator, +secondNumber);
         };
     operator = button.textContent;  //Have the button that triggers the operate function change the operator after the calculation or set operator if operator is null.
+    console.log("You've reached the end of the function")
     }
 ));
-
-
 
 function add(a,b) {
     return a + b;
@@ -101,6 +121,7 @@ function operate(number1, operatorSign, number2) {
     firstNumber = Math.round(firstNumber * 100000)/100000; // Curently rounding numbers for display purposes
     display.textContent = firstNumber;
     secondNumber = 0;
+    firstNumberBoolean = true;
     return firstNumber;
 }
 
@@ -112,3 +133,5 @@ function displayText() {
     }
 };
  
+
+console.log(regex.test(5)) //Just a test for understanding regex.  Keeping the code in for memories.
